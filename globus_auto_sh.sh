e0c0bbf00ca197a78aa6c1111cf2416f304eec86
Taylor Wade <Taylor@twair.wireless.yale.internal>
2018-07-20 11:21:23 -0400

    Bug fixes for find, endpoint activation, and updates to help menu.

--- globus_auto_sh.sh
+++ globus_auto_sh.sh
@@ -3,27 +3,31 @@
 function help_and_exit () {
 
     echo -e 'Usage:' \
-        "$0 --source-endpoint <UUID> --source-path <PATH> --shared-endpoint <UUID> --destination-path <PATH> [-d|--delete] [-h|--help]"
+        "$0 [OPTIONS] -source <UUID> -s-path <PATH> -shared <UUID> -dest-path <PATH>"
     echo ''
+    echo 'Reminder: source and destination endpoints MUST be shared. Info on creating shared endpoints:' 
+    echo '    https://docs.globus.org/how-to/share-files/'
+    echo  ''
     echo 'The following options are available:'
     echo ''
-    echo '  --source-endpoint: The endpoint UUID you want to copy data from. To search for an'
-    echo '     source endpoint, use -source "find" .'
-    echo '  -s-path: The path to the folder you want to copy to '
-    echo '    your "--shared-endpoint"'
-    echo '  --shared-endpoint: A shared endpoint UUID you have created'
+    echo '  -source || --source-endpoint: The endpoint UUID you want to copy data from. To search for'
+    echo '     a source endpoint, use -source "find" .'
+    echo '  -s-path: The path to the folder you want to copy from '
+    echo '    your "--source-endpoint"'
+    echo '  -shared || --shared-endpoint: A shared endpoint UUID you have created'
     echo '    globus.org/app/transfer by clicking "share". To search for an endpoint,'
     echo '    use -shared "find" .'
     echo '  -dest-path: The path where "--source-path" folder'
     echo '    will be copied'
-    echo '  -dir: Specifies that a folder will be recursively transferred. New folder will be created in -dest-path.'
-    echo '  --sync: Sync data between source and destination directories. Will transfer'
-    echo '    data the does not already exist and files more recent than in destination.'
+    echo '  -dir: Specifies that a folder will be recursively transferred. New folder will be created in -dest-path'
+    echo '     if -sync not specified'
+    echo '  -sync: Sync data between source and destination directories. Will transfer'
+    echo '    data the does not already exist and files more recent than in destination. Use with -dir option.'
     echo '  -find: Use to specify string to search for. Not recommended if searching for multiple endpoints.'
     echo '  -ep-scope: Used to search for endpoints, specify scope to search for endpoint.'
     echo '    Suggestions: "recently-used" (default), "my-endpoints", "shared-with-me" .'
     echo '  -l: Label for transfer'
-    echo '  -d: Delete destination folder if it already exists'
+    echo '  -del: Delete destination folder if it already exists'
     echo '  -h: Print this help message'
     echo ''
     echo "Example: $0 --source-endpoint ddb59aef-6d04-11e5-ba46-22000b92c6ec --source-path /share/godata --destination-path /shared_folder_example --shared-endpoint <your-shared-endpoint>"
@@ -71,7 +75,7 @@ while [ $# -gt 0 ]; do
         -del|--delete)
             delete='yes'
         ;;
-        --sync)
+        -sync|--sync)
             sync='yes'
         ;;
         -l|--label)
@@ -112,29 +116,47 @@ if [ $source_endpoint == "find" ]; then
 fi
 if [ $shared_endpoint == "find" ]; then
   echo "Copy and paste ID of desired shared endpoint"
-  read source_endpoint
+  read shared_endpoint
+fi
+#Check to see that endpoints are activated
+globus endpoint is-activated $source_endpoint
+
+if [ $? -ne 0 ]; then
+  echo 'Source endpoint not activated. Activating ... (if failure, will EXIT)'
+  globus endpoint activate $source_endpoint || globus endpoint activate --web $source_endpoint || exit 1
+fi
+globus endpoint is-activated $shared_endpoint
+
+if [ $? -ne 0 ]; then
+  echo 'Destination endpoint not activated. Activating ... (if failure, will EXIT)'
+  globus endpoint activate $source_endpoint || globus endpoint activate --web $source_endpoint || exit 1
 fi
 
 #Make sure paths are specified and exist
+dirname_source=`dirname "$source_path"`
+dirname_dest=`dirname "$destination_path"`
+
 if [ -z $source_path ] || [ -z $destination_path ]; then
     echo "Source path and destination path must be specified."
     exit 1
-else
-  globus ls "$shared_endpoint:$destination_path" 1>/dev/null
+elif [ -z $directory ]; then 
+  globus ls "$shared_endpoint:$dirname_dest" 1>/dev/null
   if [ $? -ne 0 ]; then
-      echo "Destination path does not exist"
+      echo "Destination directory $dirname_dest does not exist"
       exit 1
   fi
-  globus ls "$source_endpoint:$source_path" 1>/dev/null
+  globus ls "$source_endpoint:$dirname_source" 1>/dev/null
   if [ $? -ne 0 ]; then
-      echo "Source path does not exist"
+      echo "Source directory $dirname_source does not exist"
       exit 1
   fi
 fi
 
-## Do delete or sync stuffs
-basename=`basename "$source_path"`
-new_directory="$destination_path$basename"
+## Do delete or sync stuffs, if just one file then get that set up properly
+basename_source=`basename "$source_path"`
+basename_dest=`basename "$destination_path"`
+new_directory="$destination_path$basename_source"
+
 if [ -n "$directory" ]; then
     globus ls "$shared_endpoint:$new_directory" 1>/dev/null 2>/dev/null
     if [ $? -eq 0 ]; then
@@ -154,7 +176,12 @@ if [ -n "$directory" ]; then
       echo "Creating destination directory $new_directory"
       globus mkdir "$shared_endpoint:$new_directory"
     fi
+#if just one file make sure it gets transferred as one
+elif [ "$basename_source" != "$basename_dest" ]; then
+    dir=`dirname "$destination_path"`
+    destination_path="$dir$basename_source"
 fi
+
 ##do the actual transferring
 if [ -n "$directory" ]; then
     echo "transferring directory to: $new_directory"
@@ -163,7 +190,7 @@ elif [ -n "$sync" ]; then
     echo "syncing files between source $source_path and destination $destination_path"
     exec globus transfer --recursive --sync-level mtime --label "$label" "$source_endpoint:$source_path" "$shared_endpoint:$new_directory"
 else
-    echo "transferring file: $destination_path"
+    echo "transferring file to : $destination_path"
     exec globus transfer --label "$label" "$source_endpoint:$source_path" "$shared_endpoint:$destination_path"
 
 fi
